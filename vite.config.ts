268bf2b51166cdb17bb88185349ec53a016bf84a
Darshan kumar <130748235+darshan572@users.noreply.github.com>
2025-06-07 20:09:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/Portfolio_F/", // <== This is your repo name
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: "/Portfolio_F/", // ✅ Required for GitHub Pages deployment
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
